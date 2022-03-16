2d0edb97fbd9fb002c17ab9a47c271ae307198af
Kai Meikle <kaimeikle123@gmail.com>
2022-03-16 22:16:17 +0000

    completed with @LuckyKim

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  } 
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  const verticalSearch = (letters) => {
+    let returnArr = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      let newRow = [];
+      for (let j = 0; j < letters.length; j++) {
+        newRow.push(letters[j][i]);
+      }
+      returnArr.push(newRow);
+    }
+    return returnArr.map((ls) => ls.join(""));
+  }
+  
+  const verticalJoin = verticalSearch(letters);
+
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
